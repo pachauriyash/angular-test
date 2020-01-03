21fd19466b4993740846f1a968125692bf691c3c
Yash <yash@crucialbits.com>
2020-01-03 19:29:23 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { TestComponent } from "./test/test.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild(TestComponent, {static: false}) child;
+  constructor() { }
   title = 'angular-test';
+  parentMessage = "message from parent";
+  message:string;
+
+  ngAfterViewInit() {
+    this.message = this.child.message
+  }
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-test',
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
-
+  @Input() childMessage: string;
+  message = 'Hola Mundo!';
   constructor() { }
 
   ngOnInit() {
